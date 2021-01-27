427856815ef08a365dd090cda53eb3d82a1e4a5e
abhishekbarve0612 <barveabhishek.ab@gmail.com>
2021-01-27 13:22:19 +0530

    USed Class  Based API Views

--- revisingRestApis/api/urls.py
+++ revisingRestApis/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from api.views import articleList, articleDetails
+from api.views import articleList, articleDetails, ArticleAPIView, ArticleDetailsAPIView
 
 urlpatterns = [
     path('article/', articleList),
+    path('article-class/', ArticleAPIView.as_view()),
+
+    path('article-details-class/<int:id>', ArticleDetailsAPIView.as_view()),
     path('article-details/<int:pk>', articleDetails),
 ]
